1699d72ca6505dd73753fd20146483486b47d647
N0ne1eft <n0ne1eft@protonmail.com>
2023-02-26 23:58:52 +0000

    Update Event Model: EventType as enum, optional lat/lon location field

--- events/models.py
+++ events/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.utils import timezone
 import datetime
 
+class Address(models.Model):
+    lon = models.FloatField(blank=True) # Optional for Map Intergration
+    lat = models.FloatField(blank=True)
+    building_name = models.CharField(max_length = 50)
+    street_name = models.CharField(max_length = 100)
+    town_city = models.CharField(max_length = 100)
+    postcode = models.CharField(max_length = 10)
+
 class Event(models.Model):
     type = models.IntegerField(default = 0)
     title = models.CharField(max_length = 200)
@@ -10,20 +18,11 @@ class Event(models.Model):
     date = models.DateTimeField('date of event')
     attendee_count = models.IntegerField(default = 0)
     organiser = models.CharField(max_length = 100)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE)
 
-    #location - maybe using Address class or if there is a pre-existing
-    #           address class through django.
     #Image
     #rsvp
 
     #Makes events easily identifiable in admin viewer.
     def __str__(self):
         return self.title
-
-
-
-class Address(models.Model):
-    building_name = models.CharField(max_length = 50)
-    street_name = models.CharField(max_length = 100)
-    town_city = models.CharField(max_length = 100)
-    postcode = models.CharField(max_length = 10)
