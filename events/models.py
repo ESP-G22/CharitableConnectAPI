901155f288ee6faa893368f99ba7509ec2b09e69
N0ne1eft <n0ne1eft@protonmail.com>
2023-02-27 00:17:47 +0000

    Update Event Model: Orgnaiser as foreign key to CCUSer

--- events/models.py
+++ events/models.py
@@ -1,27 +1,31 @@
 from django.db import models
+from user.models import CCUser
 from django.utils import timezone
 import datetime
 
 class Address(models.Model):
-    lon = models.FloatField(blank=True) # Optional for Map Intergration
-    lat = models.FloatField(blank=True)
-    building_name = models.CharField(max_length = 50)
-    street_name = models.CharField(max_length = 100)
-    town_city = models.CharField(max_length = 100)
+    lon = models.FloatField(blank=True, null=True) # Optional for Map Intergration
+    lat = models.FloatField(blank=True, null=True)
+    buildingName = models.CharField(max_length = 50)
+    streetName = models.CharField(max_length = 100)
+    townCity = models.CharField(max_length = 100)
     postcode = models.CharField(max_length = 10)
 
 class Event(models.Model):
     type = models.IntegerField(default = 0)
     title = models.CharField(max_length = 200)
     description = models.CharField(max_length = 500)
-    pub_date = models.DateTimeField('date published')
+    pubDate = models.DateTimeField('date published', auto_now_add=True)
     date = models.DateTimeField('date of event')
-    attendee_count = models.IntegerField(default = 0)
-    organiser = models.CharField(max_length = 100)
-    address = models.OneToOneField(Address, on_delete=models.CASCADE)
+    organiser = models.ForeignKey(CCUser, on_delete=models.CASCADE)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, blank=True, null=True)
 
     #Image
     #rsvp
+
+    @property
+    def attendeeCount(self):
+        return 0 # Todo generate from rsvp
 
     #Makes events easily identifiable in admin viewer.
     def __str__(self):
