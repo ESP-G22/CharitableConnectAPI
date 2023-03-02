3cbdefc1fd308535eef466dacc60a1277ee9b2d3
ch020 <chappersthepenguin@gmail.com>
2023-03-02 14:18:13 +0000

    Added documentation
    Added basic API documentation to the server (in both Swagger and ReDoc)

--- CharitableConnectAPI/urls.py
+++ CharitableConnectAPI/urls.py
@@ -15,9 +15,29 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# Gets the schema view for the documentation
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Charitable Connect API",
+        default_version='v1',
+        description="The Django API for the Charitable Connect application",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+    url="https://api.cc.n0ne1eft.dev/",
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
-    path('events/', include('events.urls'))
+    path('events/', include('events.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
