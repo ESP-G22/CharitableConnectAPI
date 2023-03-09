20811095efbd5ef984143dd596e029b2be9db85a
N0ne1eft <n0ne1eft@protonmail.com>
2023-03-09 18:14:57 +0000

    Fix testing user authentication + templates

--- events/tests.py
+++ events/tests.py
@@ -1,39 +1,35 @@
 import datetime
 import json
-from django.test import TestCase
-from django import forms
-
-from django.contrib.auth.models import User
-from django.test.client import Client
-from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
-
-from events.models import Event, Address
 from events.serializers import *
-from user.views import CCUserProfile
-
-
-user = CCUser.objects.get(username='admin')
-client = APIClient()
-client.force_authenticate(user=user)
-
+from user.models import CCUser
 test_date = datetime.datetime(2023, 4, 15, 13, 43, 32, 10000)
 
 class EventTestCase(APITestCase):
+    def test_user_login(self):
+        """
+        Copied from User Tests, create a test user and return pk and token
+        """
+        user = CCUser.objects.create_user(username='test_user1', email="test@test.com", password='very-strong-psw')
+        login = {
+            "username": "test_user1",
+            "password": "very-strong-psw",
+        }
+        response = self.client.post("/user/login", login, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        return user.pk, response.json()['token']
 
     def test_event_create(self):
+        pk, token = self.test_user_login()
         data = { "type": "0", 
                 "title": "Test_Event", 
                 "description": "This event is going to be fun!", 
                 "date": str(test_date) }
-        json_data = json.dumps(data, indent=4)
-        response = client.post("/events/create", json_data, content_type="application/json")
+        response = self.client.post("/events/create", data, format='json', HTTP_AUTHORIZATION=f'Token {token}')
+        # TODO: Test response JSON is in expected format
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_event_view(self):
-            response = client.get("/events/0")
-            self.assertEqual(response.status_code, status.HTTP_200_OK)
+        return
   
 
 
