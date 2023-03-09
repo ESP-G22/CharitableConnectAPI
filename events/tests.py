fd6e37885e7d4a2f6e1c205c2086fb3f0842dc72
James Macintyre <94534039+JPMacintyre1@users.noreply.github.com>
2023-03-09 15:48:43 +0000

    Added some user tests
    Added more tests and fixed one of them, lots of errors still.

--- events/tests.py
+++ events/tests.py
@@ -1,31 +1,40 @@
 import datetime
 import json
 from django.test import TestCase
+from django import forms
 
 from django.contrib.auth.models import User
-from django.urls import reverse
+from django.test.client import Client
 from rest_framework.authtoken.models import Token
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
 
 from events.models import Event, Address
 from events.serializers import *
-from user.models import CCUser
+from user.views import CCUserProfile
 
 
-class CreateEventTestCase(APITestCase):
+user = CCUser.objects.get(username='admin')
+client = APIClient()
+client.force_authenticate(user=user)
 
-    # Test to create user and store token for event tests.
+test_date = datetime.datetime(2023, 4, 15, 13, 43, 32, 10000)
 
-    def test_create(self):
-        data = {"type": 1, "title": "Test_Event",
-                "description": "This event is going to be fun!",
-                "date": datetime.date.fromisocalendar(2023, 42, 3)}
-        response = self.client.post("/events/create", data)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+class EventTestCase(APITestCase):
 
-
-class EventViewTestCase(APITestCase):
-    def test_view(self):
-        response = self.client.get("/events/0")
+    def test_event_create(self):
+        data = { "type": "0", 
+                "title": "Test_Event", 
+                "description": "This event is going to be fun!", 
+                "date": str(test_date) }
+        json_data = json.dumps(data, indent=4)
+        response = client.post("/events/create", json_data, content_type="application/json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_event_view(self):
+            response = client.get("/events/0")
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+  
+
+
+
--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,36 @@
+import datetime
+import json
 from django.test import TestCase
+from django import forms
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from django.test.client import Client
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+
+from user.views import *
+from user.models import *
+
+class UserTestCase(APITestCase):
+
+    client = APIClient()
+    #client.force_authenticate(user=CCUser.objects.get(username='admin'))
+
+    # Register new user
+    def test_user_register(self):
+        data = { "username": "test_user1", 
+                "email": "testUser@mail.com", 
+                "password": "very-strong-psw"}
+        json_data = json.dumps(data, indent=4)
+        response = self.client.post("/user/register", json_data, content_type="application/json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_view_profile(self):
+        response = self.client.get("/user/profile/1", content_type="application/json")
+        print(response.json())
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+
+
