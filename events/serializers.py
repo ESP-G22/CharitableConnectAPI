3fbb90a2771b08a6f2dea3b861e92104f245ff38
N0ne1eft <n0ne1eft@protonmail.com>
2023-03-15 12:48:50 +0000

    Feat: RSVP info for requesting user in get event endpoint

--- events/serializers.py
+++ events/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
 from .models import *
 from user.serializers import CCUserProfileSerializer
+from rsvp.models import RSVP
+from rsvp.serializer import CCRSVPSerializer
 class CCEventSerializer(serializers.ModelSerializer):
     organiser = CCUserProfileSerializer(read_only=True)
     attendeeCount = serializers.IntegerField()
+    rsvp = serializers.SerializerMethodField(method_name='check_rsvp')
+
+    def check_rsvp(self, event):
+        if 'user_id' in self.context:
+            rsvp = event.rsvp_set.filter(user=self.context['user_id'])
+            if rsvp.exists():
+                return CCRSVPSerializer(rsvp[0]).data
+        return None
+
     class Meta:
         model = Event
         fields = '__all__'
